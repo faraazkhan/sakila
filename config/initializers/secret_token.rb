59861ab2d602f426ef823d4ce6e3f32510f64e19
Faraaz Khan <faraaz@rationalizeit.us>
2012-07-29 14:34:26 -0400

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Sakila::Application.config.secret_token = 'a357df6e63293a5c2846958fecb8f29c8cca8277dae88359fbcf4ec239999e9a27c8a52442eee9e22361b5bc9bd451b9451f9ec00f363c06e1d698e4133d331f'
